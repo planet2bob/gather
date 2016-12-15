f7c44f966d834cf735deb55c3db52b212eeabe00
Christian Wu <jchristian99@gmail.com>
2016-12-15 10:46:13 -0800

    more form submission

--- server/server.py
+++ server/server.py
@@ -8,6 +8,12 @@ def hello():
 def login():
     return render_template('login.html')
 
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      #do checks
+      return render_template("index.html",result = result)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
