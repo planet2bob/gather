a4447de3befda306936cb05e93c9ed8d95123807
WhimsicottMoon <tangv@bishops.com>
2017-01-07 12:30:15 -0800

    Super class for other API stuffs
    This is the class that we're planning to have the other API programs
    inherit from!  :)

---
+++ apis/Service.py
@@ -0,0 +1,21 @@
+class Service:
+
+    client = None
+
+    def _init_(self, username, password):
+        #stuff goes here
+        pass
+    
+    def send_message(client, recipient, message):
+        #stuff goes here
+        return True #whether or not message sending succeeded
+    
+    def get_contacts(client):
+        #more stuff goes here
+        contact_list = []
+        return contact_list
+        
+    def get_conversation(client, recipient):
+        #more stuff goes here
+        message_list = []
+        return message_list
