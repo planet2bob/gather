9470d52b6364d681369b6978d11124ca50e13764
WhimsicottMoon <tangv@bishops.com>
2017-01-10 11:40:57 -0800

    Renamed a couple things
    I'm terrible at naming and client != username. :/

--- apis/Service.py
+++ apis/Service.py
@@ -6,16 +6,16 @@ class Service:
         #stuff goes here
         pass
     
-    def send_message(client, recipient, message):
+    def send_message(username, recipient, message):
         #stuff goes here
         return True #whether or not message sending succeeded
     
-    def get_contacts(client):
+    def get_contacts(username):
         #more stuff goes here
         contact_list = []
         return contact_list
         
-    def get_conversation(client, recipient):
+    def get_conversation(username, recipient):
         #more stuff goes here
         message_list = []
         return message_list
