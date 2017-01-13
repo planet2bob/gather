744328b3e662e21b003153a9cc78bb4a4eca4606
Maxsun <maxlouiesun@gmail.com>
2017-01-13 09:58:54 -0800

    Added recieving test messages from server

--- server/server.py
+++ server/server.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template, request
+import time, threading
+from flask import Flask, render_template, request, session
+from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__, template_folder='./html')
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode='threading')
+
+clients = []
+
 @app.route("/")
 def hello():
     return render_template('index.html')
@@ -26,5 +33,36 @@ def result():
 def about():
     return render_template('about.html')
 
+@socketio.on('requestMessages')
+def send_messages(data):
+    username = data['username']
+    password = data['password']
+    print username, password
+    # RETRIEVE UNSENT MESSAGES FROM DB USING USERNAME + PASSWORD
+    messages_to_send = [{
+        'message': 'hello?',
+        'sender': 'maxsun',
+        'source': 'skype'
+    },
+    {
+        'message': 'you there?',
+        'sender': 'maxsun',
+        'source': 'skype'
+    },
+    {
+        'message': ':(',
+        'sender': 'maxsun',
+        'source': 'skype'
+    }]
+    for message in messages_to_send:
+        socketio.emit('message', message)
+
+@socketio.on('connected')
+def handle_message(data):
+    username = data['username']
+    password = data['password']
+    print username, password
+    print 'connected'
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
