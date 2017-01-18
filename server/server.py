d0de56d416a19171dd885f8018b2bb137cde2dab
Maxsun <maxlouiesun@gmail.com>
2017-01-18 12:12:57 -0800

    Added sending messages to server from client, recieving messages from server, and rendering messages for each contact

--- server/server.py
+++ server/server.py
@@ -1,12 +1,21 @@
 import time, threading
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, jsonify
 from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__, template_folder='./html')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode='threading')
 
-clients = []
+clients = {}
+
+class Socket:
+    def __init__(self, sid):
+        self.sid = sid
+        self.connected = True
+
+    # Emits data to a socket's unique room
+    def emit(self, event, data):
+        emit(event, data, room=self.sid)
 
 @app.route("/")
 def hello():
@@ -21,6 +30,19 @@ def checkLogin():
     email = request.json['email']
     pwd = request.json['pwd']
     return pwd
+
+@app.route("/send", methods=['POST'])
+def sendMessage():
+    message = request.form['message']
+    sender = request.form['sender']
+    source = request.form['source']
+    recipient = request.form['recipient']
+    return jsonify({
+        'message': message,
+        'sender': sender,
+        'source': source,
+        'recipient': recipient
+    })
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
@@ -41,21 +63,25 @@ def send_messages(data):
     # RETRIEVE UNSENT MESSAGES FROM DB USING USERNAME + PASSWORD
     messages_to_send = [{
         'message': 'hello?',
-        'sender': 'maxsun',
-        'source': 'skype'
+        'sender': 'Rachel Hong',
+        'source': 'skype',
+        'recipient': 'maxsun'
     },
     {
         'message': 'you there?',
-        'sender': 'maxsun',
-        'source': 'skype'
+        'sender': 'Andrew Wang',
+        'source': 'skype',
+        'recipient': 'maxsun'
     },
     {
         'message': ':(',
-        'sender': 'maxsun',
-        'source': 'skype'
+        'sender': 'Andrew Wang',
+        'source': 'skype',
+        'recipient': 'maxsun'
     }]
     for message in messages_to_send:
-        socketio.emit('message', message)
+        for s in clients[message['recipient']]:
+            s.emit('message', message)
 
 @socketio.on('connected')
 def handle_message(data):
@@ -63,6 +89,18 @@ def handle_message(data):
     password = data['password']
     print username, password
     print 'connected'
+    if username in clients.keys():
+        clients[username].append(Socket(request.sid))
+    else:
+        clients[username] = [Socket(request.sid)]
+
+@socketio.on('disconnect') # NEED TO SET clients[key] to null instead of []
+def disconnect():
+    for key in clients.keys():
+        for s in clients[key]:
+            if s.sid == request.sid:
+                clients[key].remove(s)
+                break
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
---
+++ server/static/js/index.js
@@ -0,0 +1,122 @@
+var messages = [{
+    'message': 'HEY BIDEN',
+    'sender': 'some-guy',
+    'source': 'slack',
+    'recipient': 'Joe "Daddy" Biden'
+}];
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+socket.on('connect', function() {
+    console.log('connected!');
+    socket.emit('connected', {
+        username: 'maxsun',
+        password: 'abcd'
+    });
+});
+
+$('.contact').click(function() {
+    console.log('changing contacts...');
+    var contactButtons = document.getElementsByClassName('contact');
+    for (var i = 0; i < contactButtons.length; i++) {
+        $(contactButtons[i]).removeClass('active');
+    }
+    $(this).addClass('active');
+    renderMessages();
+});
+
+socket.on('status', function (data) {
+    console.log(data);
+});
+
+socket.on('message', function (data) {
+    messages.push(data);
+    renderMessages();
+});
+
+function renderMessages() {
+    var activeContact = $('.active').text();
+    document.getElementById('messages-box').innerHTML = "";
+    for (var i = 0; i < messages.length; i++) {
+        if (messages[i].sender === activeContact) {
+            var row = document.createElement('div');
+            row.class = "row";
+            var message = document.createElement('div');
+            message.className = "received-message alert alert-success col-sm-9 pull-left";
+            var text = document.createElement('p');
+            text.innerText = messages[i].message;
+            message.appendChild(text);
+            var h6 = document.createElement('h6');
+            h6.className = "signature pull-right text-muted";
+            h6.innerText = messages[i].sender + ' ';
+            var logoSpan = document.createElement('span');
+            logoSpan.className = "fa fa-" + messages[i].source;
+            h6.appendChild(logoSpan);
+            message.appendChild(h6);
+            row.appendChild(message);
+            $('#messages-box').append(row);
+            $('#messages-box').scrollTop($('#messages-box')[0].scrollHeight);
+        } else if (messages[i].recipient === activeContact) {
+            var row = document.createElement('div');
+            row.class = "row";
+            var message = document.createElement('div');
+            message.className = "sent-message alert alert-success col-sm-9 pull-right";
+            var text = document.createElement('p');
+            text.innerText = messages[i].message;
+            message.appendChild(text);
+            var h6 = document.createElement('h6');
+            h6.className = "signature pull-right text-muted";
+            h6.innerText = messages[i].sender + ' ';
+            var logoSpan = document.createElement('span');
+            logoSpan.className = "fa fa-" + messages[i].source;
+            h6.appendChild(logoSpan);
+            message.appendChild(h6);
+            row.appendChild(message);
+            $('#messages-box').append(row);
+            $('#messages-box').scrollTop($('#messages-box')[0].scrollHeight);
+        }
+    }
+
+}
+
+$('.sendmethod').click(function() {
+    var method = this.id;
+    document.getElementById('currentplatform').className = "fa fa-" + method;
+});
+
+$("#own-message").keyup(function(event){
+    if(event.keyCode == 13){
+        var val = $('#own-message').val();
+        var method = document.getElementById('currentplatform').className.substring(6);
+        var messageData = {
+            message : val,
+            sender: 'maxsun',
+            source: method,
+            recipient: $('.active').text()
+        };
+        console.log(messageData);
+        messages.push(messageData);
+        $('#own-message').val('');
+        renderMessages();
+        $.ajax({
+            type: "POST",
+            url: '/send',
+            data: messageData,
+            success: function (d) {
+                console.log(d);
+            },
+            dataType: "JSON"
+        });
+    }
+});
+
+function requestMessages() {
+    socket.emit('requestMessages', {
+        username: 'maxsun',
+        password: 'abcd'
+    });
+}
+
+window.setInterval(function(){
+    requestMessages();
+}, 5000);
+
+renderMessages();
