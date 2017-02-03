59d9f97002efd8dbfe0253cd2e8c9be93e62e56a
WhimsicottMoon <tangv@bishops.com>
2017-02-02 22:00:43 -0800

    Service+GatherMessage changed, FB finalized, Github = ???
    Something happened to apis, and it seems to no longer exist - I recloned
    Gather and everything... But anyhow, fixed OOP, fbchat is fickle but I
    got it to work, and passing our username to ourselves constantly seems
    like a stupid idea so I changed that.

---
+++ api stuffs/GatherMessage.py
@@ -0,0 +1,6 @@
+class GatherMessage:
+
+    def __init__(self, body, sender, recipient):
+        self.body = body
+        self.sender = sender
+        self.recipient = recipient
---
+++ api stuffs/Service.py
@@ -0,0 +1,21 @@
+class Service:
+
+    client = None
+
+    def __init__(self, username, password):
+        #stuff goes here
+        pass
+    
+    def send_message(self, recipient, message):
+        #stuff goes here
+        return True #whether or not message sending succeeded
+    
+    def get_contacts():
+        #more stuff goes here
+        contact_list = []
+        return contact_list
+        
+    def get_conversation(recipient):
+        #more stuff goes here
+        message_list = []
+        return message_list
