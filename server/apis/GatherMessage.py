a72dfdf6e1e10ba7008e95cb1d846be8ba100931
Maxsun <maxlouiesun@gmail.com>
2017-02-07 08:26:31 -0800

    Added str, repr to gather message

--- server/apis/GatherMessage.py
+++ server/apis/GatherMessage.py
@@ -5,3 +5,10 @@ class GatherMessage(object):
         self.body = body
         self.sender = sender
         self.recipient = recipient
+
+    def __str__(self):
+        return self.sender, '->', self.body, '->', self.recipient
+
+    def __repr__(self):
+        return self.__str__()
+        
