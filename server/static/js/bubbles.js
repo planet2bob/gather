06ddd47b27b1eba507199f11bd01cd047d268bac
Christian Wu <jchristian99@gmail.com>
2017-02-27 09:29:45 -0800

    chat bubble changes

--- server/static/js/bubbles.js
+++ server/static/js/bubbles.js
@@ -1,16 +1,75 @@
 $('.chat[data-chat=person2]').addClass('active-chat');
 $('.person[data-chat=person2]').addClass('active');
-
-$('.left .person').mousedown(function(){
+$('.left .person').mousedown(function () {
     if ($(this).hasClass('.active')) {
         return false;
-    } else {
+    }
+    else {
         var findChat = $(this).attr('data-chat');
         var personName = $(this).find('.name').text();
         $('.right .top .name').html(personName);
         $('.chat').removeClass('active-chat');
         $('.left .person').removeClass('active');
         $(this).addClass('active');
-        $('.chat[data-chat = '+findChat+']').addClass('active-chat');
+        $('.chat[data-chat = ' + findChat + ']').addClass('active-chat');
     }
 });
+(function () {
+    var Message;
+    Message = function (arg) {
+        this.text = arg.text, this.message_side = arg.message_side;
+        this.draw = function (_this) {
+            return function () {
+                var $message;
+                $message = $($('.message_template').clone().html());
+                $message.addClass(_this.message_side).find('.text').html(_this.text);
+                $('.messages').append($message);
+                return setTimeout(function () {
+                    return $message.addClass('appeared');
+                }, 0);
+            };
+        }(this);
+        return this;
+    };
+    $(function () {
+        var getMessageText, message_side, sendMessage;
+        message_side = 'right';
+        getMessageText = function () {
+            var $message_input;
+            $message_input = $('.message_input');
+            return $message_input.val();
+        };
+        sendMessage = function (text) {
+            var $messages, message;
+            if (text.trim() === '') {
+                return;
+            }
+            $('.message_input').val('');
+            $messages = $('.messages');
+            message_side = message_side === 'left' ? 'right' : 'left';
+            message = new Message({
+                text: text
+                , message_side: message_side
+            });
+            message.draw();
+            return $messages.animate({
+                scrollTop: $messages.prop('scrollHeight')
+            }, 300);
+        };
+        $('.send_message').click(function (e) {
+            return sendMessage(getMessageText());
+        });
+        $('.message_input').keyup(function (e) {
+            if (e.which === 13) {
+                return sendMessage(getMessageText());
+            }
+        });
+        sendMessage('Hello Philip! :)');
+        setTimeout(function () {
+            return sendMessage('Hi Sandy! How are you?');
+        }, 1000);
+        return setTimeout(function () {
+            return sendMessage('I\'m fine, thank you!');
+        }, 2000);
+    });
+}.call(this));
