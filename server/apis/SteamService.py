459ba5398dd10e1c734989e176045d39c947299d
WhimsicottMoon <tangv@bishops.com>
2017-05-25 02:42:41 -0700

    STEAM!  :)
    I think this will work, although I didn't get to finish testing
    everything because I got locked out of Steam for logging in too many
    times.  Also, one of the modules is included because I had to manually
    rename some of the stuff in it so the two modules could work together.

---
+++ server/apis/SteamService.py
@@ -0,0 +1,67 @@
+import steamapi
+import vsteamapi
+from datetime import datetime
+from GatherService import GatherService
+
+class SteamService(GatherService):
+    
+    def __init__(self, usrnm, psswrd):
+        self.steam = steamapi.steamapi()
+        vsteamapi.core.APIConnection(api_key="98F11B21A6560EA46E01FBF3E1D4F9B8", validate_key = True)
+        status = self.steam.login(username= usrnm, password= psswrd)
+        while status != steamapi.enums.LoginStatus.LoginSuccessful:
+            if status == steamapi.enums.LoginStatus.TwoFactor:
+                token = raw_input("Two-factor Token: ")
+                status = self.steam.retry(twofactor=token)
+            elif status == steamapi.enums.LoginStatus.SteamGuard:
+                steamguard = raw_input("SteamGuard Code: ")
+                status = self.steam.retry(steamguard=steamguard)
+            elif status == steamapi.enums.LoginStatus.Captcha:
+                captcha = raw_input("CAPTCHA: ")
+                status = self.steam.retry(captcha=captcha)
+        self.me = vsteamapi.user.SteamUser(userurl = usrnm)
+        
+    def get_contacts(self):
+        steam_contacts = self.me.friends
+        names = []
+        for person in steam_contacts:
+            names.append(str(person.name))
+        return names
+    
+    def get_messages(self, contact_name):
+        steam_msgs = self.steam.chat.get_chat_history(self.get_ID(contact_name))
+        gather_msgs = []
+        for m in steam_msgs:
+            gather_msgs.append(self.convert_message(contact_name,m))
+    
+    def send_message(self, contact_name, message):
+        try:
+            self.steam.chat.login()
+            steam.chat.send_message(self.get_ID(contact_name), message)
+            self.steam.chat.logout()
+            return True
+        except:
+            return False
+
+    def get_ID(self, contact_name):
+        #gets steam id from contact name
+        dict = {}
+        for person in self.me.friends:
+            dict[person.name] = person.id
+        return dict[contact_name]
+
+    def convert_message(self, contact_name, steam_msg):
+        body = str(steam_msg.message)
+        senderID = steam_msg.steam_id
+        sender = self.get_Recipient_Name(contact_name, senderID)
+        recipient = self.get_Recipient_Name(contact_name, sender_ID, steam_msg)
+        time = self.convertDate(steam_msg.timestamp)
+        return GatherMessage.GatherMessage(body, sender, recipient, time)
+
+    def convertDate(self, tstamp):
+        return datetime.fromtimestamp(tstamp/1000)
+
+    def get_Recipient_Name(self, contact, senderID):
+        if(senderID == self.me.id):
+            return contact
+        return self.me.name
---
+++ server/apis/installingPysteam.py
@@ -0,0 +1,5 @@
+#to install pysteam/steamapi
+import sys
+sys.path.append("C:\Python27\Lib\site-packages\PySteam-master\steamapi")
+import steamapi
+
